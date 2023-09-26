b2230574664058594d9936dd89a0b363882ab2b0
yordan.mutafchiyski <yordan.mutafchiyski@gmail.com>
2023-09-26 18:24:04 +0300

    added in addToCart feature

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,16 +1,15 @@
 import CategoryItem from "../category-item/category-item.component";
-import './directory.styles.scss';
-const Directory = ({categories})=>{
-    return(
-        <div className="directory-container">
-        {categories.map((category) => {
+import "./directory.styles.scss";
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => {
         return (
           <CategoryItem key={category.id} category={category}></CategoryItem>
         );
       })}
     </div>
-    )
-}
-
+  );
+};
 
 export default Directory;
